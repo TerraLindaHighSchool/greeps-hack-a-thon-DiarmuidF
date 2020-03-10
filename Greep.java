5ffdcf8e4e620e6d2d2030927ff8aa77791ccf00
DiarmuidF <df41147@srcs.org>
2020-03-10 13:47:59 -0700

    Update Greep.java

--- Greep.java
+++ Greep.java
@@ -1,15 +1,17 @@
-import greenfoot.*;
+import greenfoot.*;  // (World, Actor, GreenfootImage, and Greenfoot)
 
 /**
  * A Greep is an alien creature that likes to collect tomatoes.
  * 
- * @author (your name here)
- * @version 0.2
+ * @author (Your Name Here)
+ * @version 0.1
  */
 public class Greep extends Creature
 {
     // Remember: you cannot extend the Greep's memory. So:
     // no additional fields (other than final fields) allowed in this class!
+    final static int HOW_MUCH_STEPS = 10;
+    final static int FULL_60= 60;
     
     /**
      * Default constructor for testing purposes.
@@ -18,6 +20,7 @@ public class Greep extends Creature
     {
         this(null);
     }
+
     
     /**
      * Create a Greep with its home space ship.
@@ -26,28 +29,53 @@ public class Greep extends Creature
     {
         super(ship);
     }
+    
 
     /**
-     * Do what a greep's gotta do.
+     * Do what a greep's gotta do
+     * 
+     * 
      */
     public void act()
     {
         super.act();   // do not delete! leave as first statement in act().
+        
         if (carryingTomato()) {
-            if (atShip()) {
-                dropTomato();
-            }
-            else {
+            int remainingSteps = getMemory();
+            if(remainingSteps == 0) {
+                spit("Purple");
                 turnHome();
-                move();
+            } else {
+                remainingSteps--;
+                setMemory(remainingSteps);
             }
+            
+            if(atShip()) {
+                dropTomato();
+                turn(180);
+            }
+            
+            if(atWater() || atWorldEdge()) {
+                setMemory(HOW_MUCH_STEPS + Greenfoot.getRandomNumber(HOW_MUCH_STEPS/1));
+                turn(Greenfoot.getRandomNumber(FULL_60));
+            }
+            move();
         }
-        else {
+        else { 
+            boolean hasPurple = seePaint("Purple");
+            if(hasPurple) {
+                turnHome();
+                turn(120);
+            }
+            
+            if(atWater() || atWorldEdge()) {
+                turn(Greenfoot.getRandomNumber(FULL_60));
+            }
             move();
             checkFood();
         }
     }
-    
+
     /**
      * Is there any food here where we are? If so, try to load some!
      */
@@ -55,20 +83,22 @@ public class Greep extends Creature
     {
         // check whether there's a tomato pile here
         TomatoPile tomatoes = (TomatoPile) getOneIntersectingObject(TomatoPile.class);
-        if (tomatoes != null) {
+        if(tomatoes != null) {
             loadTomato();
             // Note: this attempts to load a tomato onto *another* Greep. It won't
             // do anything if we are alone here.
         }
     }
 
+
     /**
      * This method specifies the name of the author (for display on the result board).
      */
     public static String getAuthorName()
     {
-        return "Anonymous";  // write your name here!
+        return "Ruhaan";  // write your name here!
     }
+
 
     /**
      * This method specifies the image we want displayed at any time. (No need 
@@ -76,11 +106,9 @@ public class Greep extends Creature
      */
     public String getCurrentImage()
     {
-        if (carryingTomato()) {
+        if(carryingTomato())
             return "greep-with-food.png";
-        }
-        else {
+        else
             return "greep.png";
-        }
     }
 }
